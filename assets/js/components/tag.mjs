cb0f130b0ae980349de22308bc0914e649e60d08
opheliagame <opheliagame@users.noreply.github.com>
2022-01-19 07:37:06 +0000

    deploy: fd945e5ed2419887cf7dbfa2b3268bc42f3858b6

--- assets/js/components/tag.mjs
+++ assets/js/components/tag.mjs
@@ -12,7 +12,7 @@ export function createTagComponent ({ createComponent, html, renderer }) {
     },
     render() {
       return html`
-        <a href="/tags/${this.props.name}/">
+        <a href="/tags/${this.props.name.toLowerCase()}/">
           <div class="pr-2 text-xs md:text-sm font-extralight">
             <span class="bg-lime-200 px-1 rounded-full border border-green-300">
               ${this.props.name}
